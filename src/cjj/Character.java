2985e513c6466ce26885052bb4fa169e8c177ec2
CCHENJiajie <34274642+CCHENJiajie@users.noreply.github.com>
2017-12-19 13:46:48 +0100

    Update Character.java

--- src/cjj/Character.java
+++ src/cjj/Character.java
@@ -7,12 +7,49 @@ public class Character {
 	private String name;
 	private int x,y;
 	private int age;
-	public Character() {
-	}
+	public Character() {}
 	public void die() {}
 	public void move(int x,int y) {}
-	public void getTemp() {}
-	public void getHumidity() {}
-	
-	
+	public int getTemp() {
+	        return temp;
+	}
+	public void setTemp(int temp) {
+		this.temp = temp;
+	}
+	public int getHumidity() {
+	       return humidity;
+	}
+	public void setHumidity(int humidity) {
+		this.humidity = humidity;
+	}
+	public int getLife() {
+		return life;
+	}
+	public void Setlife() {
+		this.life=life;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(int name) {
+		this.name = name;
+	}
+	public int getX() {
+	        return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+	        return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getAge() {
+	        return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
