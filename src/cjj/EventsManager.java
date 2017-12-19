2056c613ed8e46f6dd3e795ae1a291eeb89ce8c4
CCHENJiajie <34274642+CCHENJiajie@users.noreply.github.com>
2017-12-19 14:59:18 +0100

    Update EventsManager.java

--- src/cjj/EventsManager.java
+++ src/cjj/EventsManager.java
@@ -10,6 +10,18 @@ public class EventsManager {
 	public void generateEvents(Character[] c) {}
 	public void generateEvents() {}
 	
+	public int getProbabilityTemp() {
+	        return probabilityTemp;
+	}
+	public void setProbabilityTemp(int probabilityTemp) {
+                this.probabilityTemp = probabilityTemp;
+	}
+	public int getProbabilityHumidity() {
+	        return ProbabilityHumidity;
+	}
+	public void setProbabilityHumidity(int probabilityHumidity) {
+                this.probabilityHumidity = probabilityHumidity;
+	}
 	
 	
 }
