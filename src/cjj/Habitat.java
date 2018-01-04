387889a10a0338d6cf26ed9a3c10b33b563c51a9
chengmingyi <34274160+chengmingyi@users.noreply.github.com>
2018-01-04 13:56:00 +0100

    Update Habitat.java

--- src/cjj/Habitat.java
+++ src/cjj/Habitat.java
@@ -8,7 +8,33 @@ public class Habitat {
 	private int dimY;
 	private int posX;
 	private int posY;
+	private int nbr;
+	private Character [] chara;
 	
-	public void getTemp() {}
-	public void getHumidity() {}
+	public Habitat(){nrb=0;}
+	public int getTemp() { return temp;}
+	public int getHumidity() {return humidity;}
+	public void settemp(int t){ temp=t; }
+	public void sethumidity(int h){humidity=h;}
+	public void addchar(Character c){
+	    chara[nbr]= c;
+	    nbr++;
+	}
+	public string deletchara(Character c){
+	        if (chara==NULL){
+			return ("Il y n'a pas d'animal");
+		}else{
+			int n=nbr;
+			for (int i=0; i<nbr; i++){
+				if (chara[i]==c){
+					for(int k=i; k<(nbr-1); k++)
+					       chara[k]=chara[k+1];
+				        nbr--;
+				        return("Success!");
+					break;
+			        }
+			}
+			if (n==nbr) return("Il y n'a pas cet animal")
+		}
+	}
 }
