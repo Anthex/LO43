02ad0f657ef2aa2e434a4430ac60dd6dc7a6b6f5
chengmingyi <34274160+chengmingyi@users.noreply.github.com>
2018-01-05 11:41:01 +0100

    Update GameManager.java

--- src/cjj/GameManager.java
+++ src/cjj/GameManager.java
@@ -3,7 +3,11 @@ package cjj;
 public class GameManager implements CharacterManager,UserInteractionManager,EventsManager{
 	private string playername;
 	private Character[] characters;
+	private int probabilityTemp;
+	private int probabilityHumidity;
+	private Habitat[5] habitat;
 	
+	public GameManager
 	public void createNewWorld() {}
 	public void populate() {}
 	public void loop() {}
