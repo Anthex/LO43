07d787113042ce8a14976f3bf60256b2e91c2074
chengmingyi <34274160+chengmingyi@users.noreply.github.com>
2018-01-05 11:58:23 +0100

    Update GameManager.java

--- src/cjj/GameManager.java
+++ src/cjj/GameManager.java
@@ -7,12 +7,20 @@ public class GameManager implements CharacterManager,UserInteractionManager,Even
 	
 	public GameManager(string p){
 		playername=p;
-		habitat[0]=new Habitat(beach)
-		habitat[1]=new Habitat(beach)
-		habitat[2]=new Habitat(beach)
-		habitat[3]=new Habitat(beach)
-		habitat[4]=new Habitat(beach)
-	public void createNewWorld() {}
+		habitat[0]=new Habitat("beach");
+		habitat[1]=new Habitat("glaceland");
+		habitat[2]=new Habitat("desert");
+		habitat[3]=new Habitat("forest");
+		habitat[4]=new Habitat("river");
+	}
+	public void createNewWorld(string p) {
+		playername=p;
+		habitat[0]=new Habitat("beach");
+		habitat[1]=new Habitat("glaceland");
+		habitat[2]=new Habitat("desert");
+		habitat[3]=new Habitat("forest");
+		habitat[4]=new Habitat("river");
+	}
 	public void populate() {}
 	public void loop() {}
 	public void generateEvents() {}
